fcce32ce88ec9deb12f6265385b67aafd639373a
alejandro moreno <alejmor28@gmail.com>
2025-07-19 14:56:25 -0500

    Add files via upload

---
+++ pered.py
@@ -0,0 +1,44 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (QApplication, QWidget, QLabel, QVBoxLayout, QHBoxLayout, QMessageBox, QRadioButton)
+
+app = QApplication([])
+main_win = QWidget()
+main_win.setWindowTitle('quiz')
+
+question = QLabel('¿En que año se fundo algorithmics?')
+btn_answer1 = QRadioButton('2013')
+btn_answer2 = QRadioButton('2010')
+btn_answer3 = QRadioButton('2016')
+btn_answer4 = QRadioButton('2019')
+
+def show_win():
+    victory_win = QMessageBox()
+    victory_win.setText('Right')
+    victory_win.exec_()
+def show_lose():
+    lose_lose = QMessageBox()
+    lose_lose.setText('Lose')
+    lose_lose.exec_()
+
+
+layoutH1 = QHBoxLayout()
+layoutH2 = QHBoxLayout()
+layoutH3 = QHBoxLayout()
+layoutH1.addWidget(question, alignment = Qt.AlignCenter)
+layoutH2.addWidget(btn_answer1, alignment = Qt.AlignCenter)
+layoutH2.addWidget(btn_answer2, alignment = Qt.AlignCenter)
+layoutH3.addWidget(btn_answer3, alignment = Qt.AlignCenter)
+layoutH3.addWidget(btn_answer4, alignment = Qt.AlignCenter)
+layout_main = QVBoxLayout()
+layout_main.addLayout(layoutH1)
+layout_main.addLayout(layoutH2)
+layout_main.addLayout(layoutH3)
+main_win.setLayout(layout_main)
+
+btn_answer3.clicked.connect(show_win)
+btn_answer1.clicked.connect(show_lose)
+btn_answer2.clicked.connect(show_lose)
+btn_answer4.clicked.connect(show_lose)
+
+main_win.show()
+app.exec_()
